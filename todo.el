f91bcadcfa09c0ca5757a5dc96dfa76397945f2c
Justin Johnson <triplejay2013@gmail.com>
2018-10-28 00:33:36 -0600

    Saving stuff

--- todo.el
+++ todo.el
@@ -2,7 +2,8 @@
 ; REF: https://orgmode.org/manual/Fast-access-to-TODO-states.html#Fast-access-to-TODO-states
 (setq org-todo-keywords
       '((sequence "TODO(t)" "WAIT(n)" "|" "DONE(d)" "|" "CANCELED(c)") ; gen purpose
-      	(sequence "INPROGRESS(p)" "HELP(h)" "|" "SUBMITTED(s)"))) ; school
+      	(sequence "INPROGRESS(p)" "HELP(h)" "|" "SUBMITTED(s)") ; school
+      	(sequence "BUG(b)" "|" "FIXED(f)"))) ; work
 
 
 (setq org-todo-keyword-faces 
@@ -10,7 +11,7 @@
 	("WAIT"			:foreground "#CEF93D" :weight bold) 	; yellow
 	("DONE"			:foreground "#7BE52C" :weight bold)	; green
 	("FIXED"		:foreground "#7BE52C" :weight bold)	; green
-	("TODO"			:foreground "#7BE52C" :weight bold) 	; green
+	("TODO"			:foreground "#5A19D6" :weight bold) 	; blue
 	("INPROGRESS"		:foreground "#7BE52C" :weight bold) 	; green
 	("SUBMIITED"		:foreground "#5A19D6" :weight bold) 	; blue
 	("HELP"		:foreground "#CEF93D" :weight bold))) 	; yellow
