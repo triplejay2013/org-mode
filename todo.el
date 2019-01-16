784db448d79e4b6f78b25f326aeb2c0ec5cf1400
Justin Johnson <triplejay2013@gmail.com>
2019-01-16 15:09:28 -0700

    Made some changes to fix org mode.

--- todo.el
+++ todo.el
@@ -10,13 +10,13 @@
 
 (setq org-todo-keyword-faces 
       '(("CANCELED" 		:foreground "#CD424C" :weight bold) 	; red
-	("WAITING"			:foreground "#CEF93D" :weight bold) 	; yellow
-	("DONE"			:foreground "#7BE52C" :weight bold)	; green
-	("FIXED"		:foreground "#7BE52C" :weight bold)	; green
-	("TODO"			:foreground "#5A19D6" :weight bold) 	; blue
-	("INPROGRESS"		:foreground "#7BE52C" :weight bold) 	; green
-	("SUBMIITED"		:foreground "#5A19D6" :weight bold) 	; blue
-	("HELP"		:foreground "#CEF93D" :weight bold))) 	; yellow
+        ("WAITING"			:foreground "#CEF93D" :weight bold) 	; yellow
+        ("DONE"			:foreground "#7BE52C" :weight bold)	; green
+        ("FIXED"		:foreground "#7BE52C" :weight bold)	; green
+        ("TODO"			:foreground "#5A19D6" :weight bold) 	; blue
+        ("INPROGRESS"		:foreground "#7BE52C" :weight bold) 	; green
+        ("SUBMIITED"		:foreground "#5A19D6" :weight bold) 	; blue
+        ("HELP"		:foreground "#CEF93D" :weight bold))) 	; yellow
 
 
 ; Set up for Org-Capture: https://orgmode.org/manual/Setting-up-capture.html#Setting-up-capture
@@ -28,30 +28,30 @@
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Tasks")
          "* TODO %?\n%i\n %a\n" :clock-in t :clock-resume t)
-      ("s" "School" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "School")
-         "* TODO %?\n %i\n %a")
-      ("n" "Note" entry (file+headline "~/.emacs.d/Notebooks/notes.org" "Note")
-         "%?\n")
-      ("c" "Church" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Church")
-       "* TODO %?\n%i\n%a")
-      ("f" "Fincance" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Finances")
-       "* TODO %?\n%i\n%a")
-      ("m" "Misc" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Miscellaneous")
-       "* TODO %?\n%i\n%a")
-      ("w" "Work" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Work")
-       "* TODO %?\n%i\n%a")
-      ; Following Inspired by: http://cachestocaches.com/2016/9/my-workflow-org-agenda/
-      ; Journal - Inspired by the "Journal" template from Bernt Hansen's guide, I use this template for one-off and unscheduled personal tasks. This includes my daily lunch break or whenever I go get coffee with a friend. Unlike the other templates, this created heading is placed within the diary.org file, and beneath headings corresponding to the year-month-day. I typically just leave this open while I'm away from my desk, and it's rare that I look at these directly after they're created.
-      ("j" "Journal" entry (file+datetree "~/.emacs.d/Notebooks/journal.org")
-       "* %?\n%U\n" :clock-in t :clock-resume t)
-      ; Idea - I often have random thoughts throughout the day that have nothing to do with what I'm currently working on and likely aren't time sensitive. Whenever I have one of these ideas (whether it be inspiration for a blog post, or an idea for my work), I use this capture template.
-      ("i" "Idea" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Ideas")
-       "* %? :IDEA: \n%t" :clock-in t :clock-resume t)
-      ; Next Task - Whenever I have something I quickly need to get done, including a one-off errand or responding to an email, I use this. It creates a heading with the NEXT keyword and (if that weren't enough) a deadline for the end of the day. I try to ensure that all of these are done before I go to bed if possible.
-      ("n" "Next Task" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Next")
-       "** NEXT %? \nDEADLINE: %t")
-      ; Add other custom templates
-      ))
+        ("s" "School" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "School")
+           "* TODO %?\n %i\n %a")
+        ("n" "Note" entry (file+headline "~/.emacs.d/Notebooks/notes.org" "Note")
+           "%?\n")
+        ("c" "Church" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Church")
+         "* TODO %?\n%i\n%a")
+        ("f" "Fincance" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Finances")
+         "* TODO %?\n%i\n%a")
+        ("m" "Misc" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Miscellaneous")
+         "* TODO %?\n%i\n%a")
+        ("w" "Work" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Work")
+         "* TODO %?\n%i\n%a")
+        ; Following Inspired by: http://cachestocaches.com/2016/9/my-workflow-org-agenda/
+        ; Journal - Inspired by the "Journal" template from Bernt Hansen's guide, I use this template for one-off and unscheduled personal tasks. This includes my daily lunch break or whenever I go get coffee with a friend. Unlike the other templates, this created heading is placed within the diary.org file, and beneath headings corresponding to the year-month-day. I typically just leave this open while I'm away from my desk, and it's rare that I look at these directly after they're created.
+        ("j" "Journal" entry (file+datetree "~/.emacs.d/Notebooks/journal.org")
+         "* %?\n%U\n" :clock-in t :clock-resume t)
+        ; Idea - I often have random thoughts throughout the day that have nothing to do with what I'm currently working on and likely aren't time sensitive. Whenever I have one of these ideas (whether it be inspiration for a blog post, or an idea for my work), I use this capture template.
+        ("i" "Idea" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Ideas")
+         "* %? :IDEA: \n%t" :clock-in t :clock-resume t)
+        ; Next Task - Whenever I have something I quickly need to get done, including a one-off errand or responding to an email, I use this. It creates a heading with the NEXT keyword and (if that weren't enough) a deadline for the end of the day. I try to ensure that all of these are done before I go to bed if possible.
+        ("n" "Next Task" entry (file+headline "~/.emacs.d/Notebooks/todo.org" "Next")
+         "** NEXT %? \nDEADLINE: %t")
+        ; Add other custom templates
+        ))
 
 ; Clocking REF: http://cachestocaches.com/2016/9/my-workflow-org-agenda/
 ;; Set default column view headings: Task Total-Time Time-Stamp
